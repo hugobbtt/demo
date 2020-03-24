f25a235ffae1ddc883ec7c3ab6a643a9ff2a7d79
hugo <hugobbtt@gmail.com>
2020-03-24 09:34:43 +0800

    增加了订单超时取消任务

---
+++ src/main/java/com/example/demo/component/OrderTimeOutCancelTask.java
@@ -0,0 +1,20 @@
+package com.example.demo.component;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * 订单超时取消并解锁库存的定时器
+ */
+@Component
+public class OrderTimeOutCancelTask {
+    private Logger LOGGER = LoggerFactory.getLogger(OrderTimeOutCancelTask.class);
+
+    @Scheduled(cron = "0 0/10 * ? * ?")
+    private void cancelTimeOutOrder() {
+        // todo: 调用取消订单的方法
+        LOGGER.info("取消订单，并根据sku编号释放锁定库存");
+    }
+}
